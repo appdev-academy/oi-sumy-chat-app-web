5caa616cce5d7d248845c42f64f666a3b9738def
Maksym Skliarov <maksym.skliarov@gmail.com>
2025-05-01 19:24:01 +0300

    Added Sign Up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  allow_unauthenticated_access only: [ :new, :create ]
+  before_action :resume_session, only: [ :new, :create ]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to messages_path, notice: "You have signed up!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [ :email_address, :password, :password_confirmation ])
+  end
+end
