237c7490bde3f7996dfbfc66e6b9409305e4aa33
Maksym Skliarov <maksym.skliarov@gmail.com>
2025-05-09 17:23:01 +0300

    Added confetti to website

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,5 +10,6 @@ class Message < ApplicationRecord
   def broadcast_changes
     Turbo::StreamsChannel.broadcast_append_to 'chat', target: 'messages', partial: 'messages/message', locals: { message: self }
     Turbo::StreamsChannel.broadcast_action_to 'chat', action: 'scroll_to', target: "message_#{self.id}"
+    Turbo::StreamsChannel.broadcast_update_to 'chat', target: 'confetti', partial: 'shared/confetti'
   end
 end
