0d6671ab3461a967896d4ae5355180098661a428
Maksym Skliarov <maksym.skliarov@gmail.com>
2025-05-09 16:35:10 +0300

    Moved "new Message" broadcasting from controller to model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,14 @@
 class Message < ApplicationRecord
+  after_commit :broadcast_changes, on: :create
+
   belongs_to :user
 
   validates :content, presence: true
+
+  private
+
+  def broadcast_changes
+    Turbo::StreamsChannel.broadcast_append_to 'chat', target: 'messages', partial: 'messages/message', locals: { message: self }
+    Turbo::StreamsChannel.broadcast_action_to 'chat', action: 'scroll_to', target: "message_#{self.id}"
+  end
 end
